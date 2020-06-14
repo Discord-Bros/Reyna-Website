4f81860e570d0434d87900f1fa7969c44b255ab0
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-06-14 19:48:05 +0700

    added user,moderation,music folder

--- website/index.php
+++ website/index.php
@@ -3,13 +3,28 @@
 <html>
     <head>
     <title>Reyna Discord Bot</title>
-    <link rel="icon" type="img/png"href="http://localhost/Reyna/img/reyna.webp"/>
+    <link rel="icon" type="img/png"href="https://reyna-discordbot.000webhostapp.com/images/reyna.webp"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script>
+    var isNS = (navigator.appName == "Netscape") ? 1 : 0;
+    if(navigator.appName == "Netscape") document.captureEvents(Event.MOUSEDOWN||Event.MOUSEUP);
+    function mischandler(){
+    return false;
+    }
+    function mousehandler(e){
+    var myevent = (isNS) ? e : event;
+    var eventbutton = (isNS) ? myevent.which : myevent.button;
+    if((eventbutton==2)||(eventbutton==3)) return false;
+    }
+    document.oncontextmenu = mischandler;
+    document.onmousedown = mousehandler;
+    document.onmouseup = mousehandler;
+  </script>
     </head>
     <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
 <a class="navbar-brand" href="#">
-    <img src="http://localhost/Reyna/img/reyna.webp" width="30" height="30" alt="" loading="lazy">
+    <img src="https://reyna-discordbot.000webhostapp.com/images/reyna.webp" width="30" height="30" alt="" loading="lazy">
   </a>
   <a class="navbar-brand" href="#">Reyna</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,14 +45,41 @@
     <span class="navbar-text">
    <!-- Welcome, //<?php //echo $_SESSION['username']; ?>!
     <a href="../logout.php">Logout</a>-->
-    <button onclick="OAUTH2();">Login</button>
+    <button onclick="OAUTH2();">Add Now</button>
     </span>
   </div>
 </nav>
+<div class="background">
+<img src="https://reyna-discordbot.000webhostapp.com/images/Reyna_Background.png" alt="Reyna">
+</div>
 <div class="footer">
 
 </div>
      <br/>
+  <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.min.js"></script>
+  <script language="javascript">
+      $(function() {
+          toastr.options = {
+             "closeButton": true,
+             "debug": false,
+             "positionClass": "toast-bottom-right",
+             "showDuration": "300",
+             "hideDuration": "1000",
+             "timeOut": "0",
+             "extendedTimeOut": "1000",
+             "showEasing": "swing",
+             "hideEasing": "linear",
+             "showMethod": "fadeIn",
+             "hideMethod": "fadeOut"
+          }
+    toastr.info("<p'>Cookies help us deliver our services. By using our services, you agree to our use of cookies. <a href='#' target='_new'>More details</a></p>");
+      });
+  </script>
+   <link rel="stylesheet" href="http://localhost/Reyna/css/pages/images.css"/>
+   <link rel="stylesheet" href="http://localhost/Reyna/css/pages/scrollbar.css"/>
+     <script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js"></script>
+     <link rel="stylesheet" href = "http://localhost/Reyna/css/pages/cookies-agreement.css"/>
      <link rel="stylesheet" href = "http://localhost/Reyna/css/pages/font.css"/>
      <link rel="stylesheet" href="http://localhost/Reyna/css/pages/footer.css"
      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
