ce52fd194691415ff8c312464e78a80e4a797845
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-06-14 14:13:42 +0700

    init project

---
+++ website/js/event/button.js
@@ -0,0 +1,9 @@
+function OAUTH2()  {
+    var url = "https://discord.com/api/oauth2/authorize?client_id=718328949620473927&permissions=0&redirect_uri=https%3A%2F%2Freyna-discordbot.000webhostapp.com%2F&response_type=code&scope=bot%20identify%20email%20guilds";
+    window.open(url);
+}
+
+function supportLink() {
+    var supportPage = "";
+    window.open(supportPage);
+}
